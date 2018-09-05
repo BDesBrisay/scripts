9e691f2894e7d93455d0e2b57c4b1264c835c32e
Bryce DesBrisay <bryce@desbrisay.com>
2018-09-04 21:48:26 -0600

    got links from pinch of yum

--- recipes.js
+++ recipes.js
@@ -1,20 +1,30 @@
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 
-
-
 async function main() {
   try {
-    let res = await fetch('https://github.com/');
-    let text = await res.text();
+    let links = [];
 
-    const $ = cheerio.load(text);
-    console.log($.html());
-    console.log('hello')
+    for (let i = 1; i <= 56; i++) {
+      await getRecipePage(i, links);
+    }
+
+    //add links to file
+    console.log(links)
   }
   catch (e) {
     console.error(e);
   }
+}
+
+async function getRecipePage(i, links) {
+  const res = await fetch(`https://pinchofyum.com/recipes?fwp_paged=${i}`);
+  const text = await res.text();
+  const $ = cheerio.load(text);
+
+  $('.block-link').each((i, elem) => {
+    links.push($(elem).attr('href'));
+  })
 }
 
 main();
