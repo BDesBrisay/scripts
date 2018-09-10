bbeba6139838fab8fa6d4c3c63b070e10499564c
BDesBrisay <bryce@desbrisay.com>
2018-09-09 19:10:31 -0600

    got recipe links from saveur

--- saveur.js
+++ saveur.js
@@ -2,14 +2,40 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const main = async () => {
-    getLinks(1, []);
+    let links = [];
+
+    for (let i = 1; i <= 2; i++) {
+        await getLinks(i, links);
+    }
+
+    links = [].concat.apply([], links);
+    console.log(links)
 }
 
 const getLinks = async (i, links) => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 1000, height: 600 });
-    await page.goto(`https://www.saveur.com/recipes-search?page=2`)
+    await page.goto(`https://www.saveur.com/recipes-search?page=${i}`)
+    console.log('loaded');
+
+    const result = await page.evaluate(() => {
+        let urls = [];
+        let items = document.querySelectorAll('.result_title a');
+
+        items.forEach((item) => {
+            try {
+                urls.push(`https://www.saveur.com${item.getAttribute('href')}`);
+            }
+            catch (e) {
+                console.error(e);
+            }
+        });
+
+        return urls;
+    });
+
+    links.push(result);
+    browser.close();
 }
 
 main();
