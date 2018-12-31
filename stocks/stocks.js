49cbf57716cbfe7429074d55053b7efd12b8d94d
Bryce DesBrisay <bryce@desbrisay.com>
2018-12-31 03:03:27 -0800

    added new chart and now am only testing on new data

--- stocks/stocks.js
+++ stocks/stocks.js
@@ -15,23 +15,23 @@ const scaleDown = (obj) => ({
 });
 */
 
+const AVKEY1 = '9RUKGUEQ3Y8VOFG9';
+const AVKEY2 = 'E6BKK6T99RPFV58P';
 const scale = 250;
 
 const normalize = (obj) => {
   const gain = obj.close > obj.open ? 1 : 0;
   return { 
     input: { 
-      // date: obj.date,
-      // open: obj.open / scale, 
-      // vwapDiff: obj.open - obj.vwap, 
       aboveSAR: obj.open > obj.sar,
       macd: obj.macd,
       apo: obj.apo,
       rsi: obj.rsi / 100,
-      // mom: obj.mom,
       underSMA50: obj.open < obj.sma50,
-      //belowSMA100: obj.open < obj.sma100,
-      //belowSMA200: obj.open < obj.sma200
+      belowSMA100: obj.open < obj.sma100,
+      belowSMA200: obj.open < obj.sma200,
+      mom: obj.mom,
+      // open: obj.open / scale, // previous close? 
     }, 
     output: [ gain ]
   }
@@ -40,64 +40,59 @@ const normalize = (obj) => {
 const getData = async (symbol) => {
   try {  
     const IEX = await fetch(
-      `https://api.iextrading.com/1.0/stock/${symbol}/chart/2y`, 
+      `https://api.iextrading.com/1.0/stock/${symbol}/chart/5y`, 
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
     
-    /*const daily = await fetch(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=9RUKGUEQ3Y8VOFG9`, 
-      { method: 'get', headers: { 'Content-Type': 'application/json' } }
-    ).then(res => res.json());*/
-
     const AVSAR = await fetch(
-      `https://www.alphavantage.co/query?function=SAR&symbol=${symbol}&interval=daily&acceleration=0.05&maximum=0.25&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=SAR&symbol=${symbol}&interval=daily&acceleration=0.05&maximum=0.25&apikey=${AVKEY1}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
 
     const AVMACD = await fetch(
-      `https://www.alphavantage.co/query?function=MACD&symbol=${symbol}&interval=daily&series_type=open&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=MACD&symbol=${symbol}&interval=daily&series_type=open&apikey=${AVKEY1}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
 
     const AVSMA50 = await fetch(
-      `https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=daily&time_period=50&series_type=open&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=daily&time_period=50&series_type=open&apikey=${AVKEY1}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
 
     const AVRSI = await fetch(
-      `https://www.alphavantage.co/query?function=RSI&symbol=${symbol}&interval=daily&time_period=10&series_type=open&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=RSI&symbol=${symbol}&interval=daily&time_period=10&series_type=open&apikey=${AVKEY1}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
 
     const AVAPO = await fetch(
-      `https://www.alphavantage.co/query?function=APO&symbol=${symbol}&interval=daily&series_type=close&fastperiod=10&matype=1&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=APO&symbol=${symbol}&interval=daily&series_type=close&fastperiod=10&matype=1&apikey=${AVKEY1}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
-
-    /*
-    Other Indicator API Calls
-
+    
     const AVSMA100 = await fetch(
-      `https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=daily&time_period=100&series_type=open&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=daily&time_period=100&series_type=open&apikey=${AVKEY2}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
 
     const AVSMA200 = await fetch(
-      `https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=daily&time_period=200&series_type=open&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=daily&time_period=200&series_type=open&apikey=${AVKEY2}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
 
     const AVMOM = await fetch(
-      `https://www.alphavantage.co/query?function=MOM&symbol=${symbol}&interval=daily&series_type=close&time_period=10&apikey=9RUKGUEQ3Y8VOFG9`,
+      `https://www.alphavantage.co/query?function=MOM&symbol=${symbol}&interval=daily&series_type=close&time_period=10&apikey=${AVKEY2}`,
       { method: 'get', headers: { 'Content-Type': 'application/json' } }
     ).then(res => res.json());
-    */
 
-    
     /*
-    if (AVSMA100['Technical Analysis: SMA'] && AVSMA100['Technical Analysis: SMA'][item.date]) item.sma100 = Number(AVSMA100['Technical Analysis: SMA'][item.date]['SMA']);
-    if (AVSMA200['Technical Analysis: SMA'] && AVSMA200['Technical Analysis: SMA'][item.date]) item.sma200 = Number(AVSMA200['Technical Analysis: SMA'][item.date]['SMA']);
-    // if (AVMOM['Technical Analysis: MOM'] && AVMOM['Technical Analysis: MOM'][item.date]) item.mom = Number(AVMOM['Technical Analysis: MOM'][item.date]['MOM']);
+    Daily information from Alpha Vantage
+
+    const daily = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=${AVKEY1}`, 
+      { method: 'get', headers: { 'Content-Type': 'application/json' } }
+    ).then(res => res.json());
+
+    Other Indicator API Calls
     */
 
     const newData = IEX.map((item, i) => {
@@ -106,6 +101,9 @@ const getData = async (symbol) => {
       if (AVSMA50['Technical Analysis: SMA'] && AVSMA50['Technical Analysis: SMA'][item.date]) item.sma50 = Number(AVSMA50['Technical Analysis: SMA'][item.date]['SMA']);
       if (AVRSI['Technical Analysis: RSI'] && AVRSI['Technical Analysis: RSI'][item.date]) item.rsi = Number(AVRSI['Technical Analysis: RSI'][item.date]['RSI']);
       if (AVAPO['Technical Analysis: APO'] && AVAPO['Technical Analysis: APO'][item.date]) item.apo = Number(AVAPO['Technical Analysis: APO'][item.date]['APO']);
+      if (AVSMA100['Technical Analysis: SMA'] && AVSMA100['Technical Analysis: SMA'][item.date]) item.sma100 = Number(AVSMA100['Technical Analysis: SMA'][item.date]['SMA']);
+      if (AVSMA200['Technical Analysis: SMA'] && AVSMA200['Technical Analysis: SMA'][item.date]) item.sma200 = Number(AVSMA200['Technical Analysis: SMA'][item.date]['SMA']);
+      if (AVMOM['Technical Analysis: MOM'] && AVMOM['Technical Analysis: MOM'][item.date]) item.mom = Number(AVMOM['Technical Analysis: MOM'][item.date]['MOM']);
       return item;
     });
 
@@ -131,10 +129,10 @@ const main = async () => {
     const rawData = await getData(symbol);
 
     const trainingData = rawData.map(normalize);
-    const net = new brain.NeuralNetwork({ hiddenLayers: [12] })
+    const net = new brain.NeuralNetwork({ hiddenLayers: [14] })
 
     content += '<p>Training...</p>';
-    net.train(trainingData.slice(0, trainingData.length - 126), {
+    net.train(trainingData.slice(0, trainingData.length - 252), {
       iterations: 75000
     });
 
@@ -145,7 +143,7 @@ const main = async () => {
     let error = 0;
     let loss = 0;
     let gain = 0;
-    let value = 10000;
+    let value = 5000;
     for (let i = trainingData.length - 252; i < trainingData.length; i++) {
       const res = net.run(trainingData[i].input);
       const truth = Math.round(res) === trainingData[i].output[0];
@@ -169,7 +167,7 @@ const main = async () => {
     const correct = results.filter(item => item === true).length;
     content += `<p>Accuracy: <strong>${(100 * correct / results.length).toFixed(0)}%</strong> (${correct} / ${results.length})</p>`;
     content += `<p>Avg. Error: <strong>${(error / results.length).toFixed(5)}%</strong> (${error.toFixed(5)} / ${results.length})</p>`;
-    content += `<p>Starting Value of $10000 Over 1 year: <strong style="${value > 0 ? 'color: green' : 'color: red'}">$${value.toFixed(2)}</strong></p>`;
+    content += `<p>Starting Value of $5000 Over 1 year: <strong style="${value > 0 ? 'color: green' : 'color: red'}">$${value.toFixed(2)}</strong></p>`;
     content += `<p>Percent Profit: <strong style="${gain - loss > 0 ? 'color: green' : 'color: red'}">${(value / 100).toFixed(2)}%</strong>`;
 
     // Test Input
@@ -182,7 +180,7 @@ const main = async () => {
     console.error(e);
   }
   const diff = Date.now() - start;
-  console.log(diff)
+  console.log('Diff', diff)
 }
 
 main()
