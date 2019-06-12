eee5ecf3b4fb800506a8676efdad500ae3d4518c
BDesBrisay <bryce@desbrisay.com>
2019-06-12 10:02:49 -0700

    removed sort category links

--- scraping/deals.js
+++ scraping/deals.js
@@ -30,13 +30,16 @@ async function getDeal(links) {
   const text = await res.text();
   const $ = cheerio.load(text);
 
-  const regex = /\/deals\//g;
+  const deal = /\/deals\//g;
+  const sort = /\/deals\/by\//g;
 
   $('a').each((i, elem) => {
     const url = $(elem).attr('href');
-    const isDeal = url.match(regex);
+
+    const isDeal = url.match(deal);
+    const isSort = url.match(sort);
     
-    if (isDeal) {
+    if (isDeal && !isSort) {
       links.push(url);
     }
   })
