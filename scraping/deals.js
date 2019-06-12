5103579e2cdc00082c8e02384c3f7509a865bcbc
BDesBrisay <bryce@desbrisay.com>
2019-06-12 09:21:26 -0700

    deals links

---
+++ scraping/deals.js
@@ -0,0 +1,45 @@
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const writeFileAsync = promisify(fs.writeFile);
+
+async function main() {
+  try {
+    let links = [];
+
+    await getDeal(links);
+
+    links.forEach((link, i) => {
+      const regex = /\?position=[0-9]/g;
+      links[i] = link.replace(regex, '');
+    });
+
+    new Set(links).forEach((link) => {
+      fs.appendFile('deals.txt', `${link},\n`, () => {});
+    });
+  }
+  catch (e) {
+    console.error(e);
+  }
+}
+
+async function getDeal(links) {
+  const res = await fetch(`https://weedmaps.com/deals`);
+  const text = await res.text();
+  const $ = cheerio.load(text);
+
+  const regex = /\/deals\//g;
+
+  $('a').each((i, elem) => {
+    const url = $(elem).attr('href');
+    const isDeal = url.match(regex);
+    
+    if (isDeal) {
+      links.push(url);
+    }
+  })
+}
+
+main();
