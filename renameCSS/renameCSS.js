5c1dedb1ff2758d606aa1abbfd4b7be4d70f2638
Bryce DesBrisay <bryce@desbrisay.com>
2020-01-14 21:54:47 -0700

    finished rename css

--- renameCSS/renameCSS.js
+++ renameCSS/renameCSS.js
@@ -1,50 +1,104 @@
 const fs = require('fs');
 const readline = require('readline');
 
-function rename(filename, cssPath, htmlPath) {
-  const rl = readline.createInterface({
+function rename(filename, cssPath, htmlPath, htmlExt) {
+  const css = readline.createInterface({
     input: fs.createReadStream(cssPath + '/' + filename + '.css'),
     crflDdelay: Infinity
   });
 
   const oldClasses = [];
   const newClasses = [];
-  let newFileStr = '';
+  let newCSS = '';
 
-  rl.on('line', (content) => {
+  css.on('line', (content) => {
     let line = content;
     const match = line.match(/[\.].*[{]/g) || [];
     
     if (match.length) {
-      const oldClass = line.replace(/[^\w]/g, '');
+      const subClass = line.match(/[\.]\w+/g, '')[0];
+      const oldClass = subClass.substr(1);
       const newClass = `${filename}-${oldClass}`;
-      oldClasses.push(oldClass);
-      newClasses.push(newClass);
 
-      line = '.' + newClass + ' {';
+      if (!oldClasses.includes(oldClass)) {
+        oldClasses.push(oldClass);
+        newClasses.push(newClass);
+      }
+
+      line = line.replace(oldClass, newClass);
     }
 
-    newFileStr += line;
+    newCSS += line + '\n';
   });
 
-  rl.on('close', () => {
-    console.log(oldClasses, newClasses, newFileStr);
-
+  css.on('close', async () => {
     // write new css file to:
-    // filename-renamed.css
+    await fs.writeFile(`${cssPath}/${filename}-RENAMED.css`, newCSS, (e) => {if (e) throw(e)});
 
-    // go thought html and replace old class with new class
-    // REGEX for html classes: /class=".*CLASSNAME.*"/g
+    // go through html and replace old class with new class
+    // REGEX: /class=".*CLASSNAME.*"/g
+    const html = readline.createInterface({
+      input: fs.createReadStream(htmlPath + '/' + filename + htmlExt),
+      crflDdelay: Infinity
+    });
+  
+    let newHTML = '';
+  
+    html.on('line', (content) => {
+      let line = content;
 
-    // replace css file imports with new named ones 
-    // REGEX: ???
+      console.log(oldClasses)
+
+      // replace classes
+      // check for class="className"
+      for (let item of oldClasses) {
+        const classRegex = new RegExp('class=".*' + item + '.*"');
+        const classMatch = line.match(classRegex) || [];
+
+        if (classMatch.length) {          
+          const newClass = `${filename}-${item}`;
+          line = line.replace(item, newClass);
+        }
+      }
+
+      // replace imports
+      // check for type="text/css"
+      const cssFileRegex = new RegExp(filename + '.css');
+      const cssFileMatch = line.match(cssFileRegex) || [];
+      if (cssFileMatch.length) {
+        line = line.replace('.css', '-RENAMED.css');
+      }
+  
+      newHTML += line + '\n';
+    });
+  
+    html.on('close', async () => {
+      console.log(newHTML)
+      // create new html file
+      await fs.writeFile(`${htmlPath}/${filename}-RENAMED${htmlExt}`, newHTML, (e) => {if (e) throw(e)});
+    });
   });
+
+  console.log('FINISHED')
 }
 
-function renameCSS(names, cssPath, htmlPath) {
+function renameCSS(names, cssPath, htmlPath, htmlExt = '.html') {
   for (let name of names) {
-    rename(name, cssPath, htmlPath);
+    rename(name, cssPath, htmlPath, htmlExt);
   }
 }
 
-renameCSS(['index'], './css', './html');
+let names = [
+  'Landing',
+  'SignIn',
+  'Streams',
+  'Roulette',
+  'Buddies',
+  'Stream'
+];
+
+const cssPath = '../../blazebuddies/web/css';
+const htmlPath = '../../blazebuddies/web/components';
+const htmlExt = '.js';
+
+renameCSS(names, cssPath, htmlPath, htmlExt);
