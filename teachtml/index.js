f15233156ad83dcee23df2ada68183fbc9abfb37
Bryce DesBrisay <bryce@desbrisay.com>
2020-02-16 06:57:41 -0800

    styled tags

--- teachtml/index.js
+++ teachtml/index.js
@@ -1,9 +1,14 @@
-let dragged = null;
+let dragged;
+let draggedTag;
+
+// ELEMENT MENU
 
 function handleDragStart(e) {
   dragged = this.cloneNode(true);
   dragged.classList.remove('item');
   dragged.classList.add('contentItem');
+  dragged.draggable = '';
+  dragged.innerHTML = '';
   // e.dataTransfer.effectAllowed = 'copy';
   // e.dataTransfer.setData('text/html', e.target);
   
@@ -13,6 +18,7 @@ function handleDragStart(e) {
 function handleDragOver(e) {
   if (e.preventDefault) e.preventDefault();
 
+  this.classList.add('over');
   // e.dataTransfer.dropEffect = 'copy';
   return false;
 }
@@ -38,16 +44,43 @@ function handleDragEnd(e) {
   e.target.style.opacity = '';
 }
 
+// CONTENT ITEMS
+
+function contentDragStart(e) {
+  draggedTag = this;
+  // e.dataTransfer.effectAllowed = 'copy';
+  // e.dataTransfer.setData('text/html', e.target);
+  
+  this.style.opacity = '0.4';
+}
+
+function contentDragOver(e) {
+  e.target.classList.add('contentOver');
+}
+
+function contentDragEnd(e) {
+  console.log(e)
+  e.target.style.opacity = '';
+}
+
+// HANDLERS
+
 function itemHandlers(item) {
   item.addEventListener('dragstart', handleDragStart, false);
   item.addEventListener('dragend', handleDragEnd, false);
 }
 
-function contentHandlers(item) {
-  item.addEventListener('dragenter', handleDragEnter, false)
+function docHandlers(item) {
+  item.addEventListener('dragenter', handleDragEnter, false);
   item.addEventListener('dragover', handleDragOver, false);
   item.addEventListener('dragleave', handleDragLeave, false);
   doctype.addEventListener('drop', handleDrop, false);
+}
+
+function contentHandlers(item) {
+  item.addEventListener('dragstart', contentDragStart, false);
+  item.addEventListener('dragover', contentDragOver, false);
+  item.addEventListener('dragend', contentDragEnd, false);
 }
 
 const items = document.querySelectorAll('.item');
@@ -55,4 +88,4 @@ items.forEach(itemHandlers);
 
 const doctype = document.getElementById('doctype');
 const content = document.getElementById('content');
-contentHandlers(content);
+docHandlers(content);
