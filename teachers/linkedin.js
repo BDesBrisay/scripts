86e6b44da680b8996ab78e669f4ff5e2a9f780dd
BDesBrisay <bryce@desbrisay.com>
2020-02-27 18:04:50 -0700

    used google package

--- teachers/linkedin.js
+++ teachers/linkedin.js
@@ -1,6 +1,7 @@
 'user strict'
 
 const puppeteer = require('puppeteer');
+const google = require('google');
 const { promisify } = require('util');
 const fs = require('fs');
 
@@ -10,7 +11,11 @@ const writeFileAsync = promisify(fs.writeFile);
 
 const URL = 'https://www.google.com';
 
-const getResult = async (name, school) => {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getResult = async (i, name, school) => {
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -20,7 +25,7 @@ const getResult = async (name, school) => {
     await page.type('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input', query, { delay: 10 });
     await page.keyboard.press(String.fromCharCode(13));
 
-    console.log(name, school)
+    console.log(i, name, school)
     const wait = (Math.random() * 1000).toFixed(0);
     await page.waitFor(wait);
 
@@ -42,10 +47,34 @@ const getResult = async (name, school) => {
   }
 }
 
+async function googleResult(i, name, school) {
+  try {
+    console.log(i, name, school)
+    const query = `site:linkedin.com ${name}`;
+
+    let link = await google(query, (err, res) => {
+      if (err) console.error(err);
+      if (res && res.links) return res.links[0];
+    });
+
+    console.log(link)
+
+    return link;
+  }
+  catch (e) {
+    console.error(e);
+  }
+}
+
 async function main() {
   let data = [];
-  for (let item of teachers) {
-    const res = await getResult(item.name, item.school);
+  for (let i in teachers) {
+    const item = teachers[i];
+
+    await sleep(5000);
+    console.log(i)
+
+    const res = await googleResult(i, item.name, item.school);
     if (res) item.link = res;
 
     data.push(item);
